f169217f142630b83beeb496e561691b0570d5f2
Brian <brian.kiprono8@student.moringaschool.com>
2025-04-14 23:16:51 +0300

    expense table

--- src/components/ExpensesTable.jsx
+++ src/components/ExpensesTable.jsx
@@ -3,7 +3,27 @@ import React from 'react'
 const ExpensesTable = () => {
   return (
     <div className='table'>
-     
+      <table border="10">
+        
+        <tr>
+        <th width="100">Expenses</th>
+        <th  width="100">Description</th>
+        <th  width="100">Category</th>
+        <th  width="100">Amount</th>
+        <th width="100">Date</th>
+        </tr>
+        <tr>
+        <td width="">Expenses</td>
+        <td  width="">Description</td>
+        <td  width="">Category</td>
+        <td  width="">Amount</td>
+        <td width="">Date</td>
+        </tr>
+      
+
+      </table>
+        
+        <tr></tr>
     </div>
   )
 }
