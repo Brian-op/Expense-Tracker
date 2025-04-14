2b30f13d88511c742f1667308327cd3ad7dabf18
Brian <brian.kiprono8@student.moringaschool.com>
2025-04-15 00:42:43 +0300

    table layout

--- src/components/ExpensesTable.jsx
+++ src/components/ExpensesTable.jsx
@@ -3,27 +3,34 @@ import React from 'react'
 const ExpensesTable = () => {
   return (
     <div className='table'>
-      <table border="10">
+      <table border="10" cellPadding={10}>
+        <thead>
+         <tr>
+         <th width="100">Expenses</th>
+         <th  width="100">Description</th>
+         <th  width="100">Category</th>
+         <th  width="100">Amount</th>
+         <th width="100">Date</th>
+         </tr>
+        </thead>
+
+        <tbody>
+         <tr>
+         <td width="1">Expenses</td>
+
+          <td  width="1">Description</td>
+
+          <td  width="">Category</td>        
         
-        <tr>
-        <th width="100">Expenses</th>
-        <th  width="100">Description</th>
-        <th  width="100">Category</th>
-        <th  width="100">Amount</th>
-        <th width="100">Date</th>
-        </tr>
-        <tr>
-        <td width="">Expenses</td>
-        <td  width="">Description</td>
-        <td  width="">Category</td>
-        <td  width="">Amount</td>
-        <td width="">Date</td>
-        </tr>
-      
+          <td  width="">Amount</td>        
+
+         <td width="">Date</td>
+         </tr>
+        </tbody>
 
       </table>
-        
-        <tr></tr>
+      
+      
     </div>
   )
 }
